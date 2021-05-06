42d3c6cf4f713550e4e04183f4adc22297c29156
FerreiraBen <josiascavalcante@super.ufam.edu.br>
2021-05-06 07:47:32 -0400

    Lab 4: Integração Numérica

--- Trab 4/src/main.c
+++ Trab 4/src/main.c
@@ -3,6 +3,7 @@
 #include "integral.h"
 
 // Traditional Control Systems functions
+// Ramp
 double ramp(double t){
     const double a = 1;
     const double b = 0;
@@ -10,46 +11,57 @@ double ramp(double t){
     else return a*t + b;
 }
 
+// Heaviside
+double u(double t){
+    if(t<0) return 0;
+    else return 1;
+}
+
 // Miscelaneous
 double misc1(double t){
     return 2*(pow(t,2)) + t - 3;
 }
 
+// Trigonometric
+double misc2(double t){
+    return (cos(t) + cos(-t))/2; // Rad
+}
+
 
 int main(int argc, char const *argv[]){
-    const double delta = 0.001;
-    double y;
+    function_t f;
+    // ------ Select a Function
+    //f = u;
+    //f = misc1;
+    //f = ramp;
+    f = misc2;
 
-    for(double t = 0; t<5; t+=delta){
-        y = ramp(t);
-        printf("y = %f\n", y);
-    }
+
+    // double y;
+    // const double delta = 0.001;
+    // for(double t = 0; t<5; t+=delta){
+    //     y = f(t);
+    //     printf("y = %f\n", y);
+    // }
 
     const int A = 1;
     const int B = 2;
+    
 
     printf("The Trapezium integral between %d and %d seconds: %f\n", 
-        A,
-        B,
-        trapezium(misc1, A, B)
+        A, B, trapezium(f, A, B)
     );
 
     printf("The Left Riemann integral between %d and %d seconds: %f\n", 
-        A,
-        B,
-        Left_Riemann(misc1, A, B)
+        A, B, Left_Riemann(f, A, B)
     );
  
     printf("The Right Riemann integral between %d and %d seconds: %f\n", 
-        A,
-        B,
-        Right_Riemann(misc1, A, B)
+        A, B, Right_Riemann(f, A, B)
     );
 
     printf("The Average Riemann integral between %d and %d seconds: %f\n", 
-        A,
-        B,
-        AVG_Riemann(misc1, A, B)
+        A, B, AVG_Riemann(f, A, B)
     );
 
     return(0);
